8007a738017c1e5562fb65374ae7103b8f487b4c
VinhDang88 <vincedang720@gmail.com>
2022-07-19 18:36:41 -0400

    First commit

---
+++ MovieDataBase/Models/Movie.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieDataBase.Models
+{
+    public partial class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Genre { get; set; }
+        public double? Runtime { get; set; }
+
+        public static void SearchByGenre(string genre)
+        {
+            using (MovieDBContext context = new MovieDBContext())
+            {
+                List<Movie> result = context.Movies.Where(m => m.Genre == genre.ToLower()).ToList();
+                if (result.Count > 0)
+                {
+                    foreach (Movie movie in result)
+                    {
+                        Console.WriteLine($"Movies:{movie.Title}");
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, based on your choice we don't have that!");
+                }
+          
+            }
+        }
+        public static void SearchByTitle(string title)
+        {
+            using (MovieDBContext context = new MovieDBContext())
+            {
+                List<Movie> result = context.Movies.Where(m => m.Title == title.ToLower()).ToList();
+                if (result.Count > 0)
+                {
+                    foreach (Movie movie in result)
+                    {
+                        Console.WriteLine($"Movie: {movie.Title}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, based on your choice we don't have that!");
+                }
+            }
+        }
+    }
+}
---
+++ MovieDataBase/Program.cs
@@ -0,0 +1,140 @@
+﻿using MovieDataBase;
+using MovieDataBase.Models;
+using System.Linq;
+//Display
+Console.WriteLine("Welcome to the Movie Zone!");
+Console.WriteLine("We have two options that you can pick from to search a movie");
+Console.WriteLine("You can pick the genre or title of a movie");
+string choice = Console.ReadLine().Trim().ToLower();
+GetUserChoice(choice);
+//Methods
+static void DisplayMovie(List<Movie> newMovie)
+{
+    foreach (Movie movie in newMovie)
+    {
+        Console.WriteLine($"{movie.Title}, {movie.Genre}, {movie.Runtime}");
+    }
+}
+static void AddMovie(List<Movie> newMovie)
+{
+    using (MovieDBContext context = new MovieDBContext())
+    {
+        foreach (Movie movie in newMovie)
+        {
+            context.Movies.Add(movie);
+        }
+        //context.Movies.Add(newMovie);
+        context.SaveChanges();
+    }
+}
+static void GetUserChoice(string choice)
+{
+    bool result = true;
+    
+    while (true)
+    {
+        //string choice = Console.ReadLine().Trim().ToLower();
+        if (choice == "genre")
+        {
+            Console.WriteLine("Please pick the genre you want to watch");
+            string genre = Console.ReadLine().Trim().ToLower();
+            Movie.SearchByGenre(genre);
+            result = true;
+            break;
+        }
+        else if (choice == "title")
+        {
+            Console.WriteLine("Please pick a movie you want to watch");
+            string title = Console.ReadLine().Trim().ToLower();
+            Movie.SearchByTitle(title);
+            result = true;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("That was invalid. Try again.");
+            result = false;
+            break;
+        }
+    }
+}
+static void DisplayMovies()
+{
+    using (MovieDBContext context = new MovieDBContext())
+    {
+        foreach (Movie movie in context.Movies)
+        {
+            Console.WriteLine($"{movie.Title} {movie.Genre} {movie.Runtime}");
+        }
+    }
+}
+List<Movie> newMovie = new List<Movie>()
+{
+    new Movie()
+    {
+        Title = "Jurassic Park",
+        Genre = "Scifi",
+        Runtime = 127
+    },
+    new Movie()
+    {
+        Title = "Basket Case",
+        Genre = "Horror",
+        Runtime = 91
+    },
+    new Movie()
+    {
+        Title = "The Matrix",
+        Genre = "Scifi",
+        Runtime = 136
+    },
+    new Movie()
+    {
+        Title = "The Godfather",
+        Genre = "Crime",
+        Runtime = 175
+    },
+    new Movie()
+    {
+        Title = "The Faculty",
+        Genre = "Horror",
+        Runtime = 104
+    },
+    new Movie()
+    {
+        Title = "Friday the 13th",
+        Genre = "Horror",
+        Runtime = 95
+    },
+    new Movie()
+    {
+        Title = "Minions: The Rise of Gru",
+        Genre = "Comedy",
+        Runtime = 90
+    },
+    new Movie()
+    {
+        Title = "Forest Gump",
+        Genre = "Drama",
+        Runtime = 142
+    },
+    new Movie()
+    {
+        Title = "Shallow Hal",
+        Genre = "Comedy",
+        Runtime = 114
+    },
+    new Movie()
+    {
+        Title = "Shrek",
+        Genre = "Animated",
+        Runtime = 90
+    },
+    new Movie()
+    {
+        Title = "A Silent Voice",
+        Genre = "Anime",
+        Runtime = 129
+    },
+
+};
